83d081a9753c18923b202af2afa23b8e6b98feee
SHARIF IQBAL JAMIL <msijmpshaan369@gmail.com>
2025-06-02 10:22:31 +0530

    merging minhaj code into main branch

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,4 @@
-import ContactForm from './components/ContactForm';
-import Footer from './components/global/Footer';
-import Heading from './components/global/Heading';
-import Navbar from './components/global/Navbar';
-import Blog from './components/home/Blog';
-import ChooseUs from './components/home/ChooseUs';
-import Hero from './components/home/Hero';
-import OurTeams from './components/home/OurTeams';
-import OurWork from './components/home/OurWork';
-// import Services from './components/home/Services';
-import TechnologiesSlider from './components/home/TechnologiesSlider';
-import Testimonials from './components/home/Testimonials';
-import { BrowserRouter as Router,Routes,Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import AboutUs from './pages/AboutUs';
 import Service from './pages/Service';
@@ -24,25 +12,12 @@ function App() {
         <Routes>
           <Route path="/" element={<Layout />}>
             <Route path="" element={<Home />} />
-            <Route path="services" element={<Service/>} />
+            <Route path="services" element={<Service />} />
             <Route path="about-us" element={<AboutUs />} />
             <Route path="contact-us" element={<ContactUs />} />
           </Route>
-          
         </Routes>
       </Router>
-      {/* <Navbar /> */}
-      {/* <Hero /> 
-      {/* <Heading title="Why Choose Us" tagline="Your vision, Our Code" /> */}
-      {/* <ChooseUs />
-      <Services />
-      <TechnologiesSlider />
-      <OurWork />
-      <Testimonials />
-      <Blog />
-      <OurTeams />
-      <ContactForm/>
-      <Footer />  */}
     </div>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,20 @@
+import ContactForm from '../components/ContactForm';
+import ChooseUs from '../components/home/ChooseUs';
+import Hero from '../components/home/Hero';
+import OurTeams from '../components/home/OurTeams';
+import OurWork from '../components/home/OurWork';
+import Services from '../components/home/Services';
+
 function Home() {
   return (
-    <div>Home</div>
-  )
+    <>
+      <Hero />
+      <ChooseUs />
+      <Services />
+      <OurWork />
+      <OurTeams />
+      <ContactForm />
+    </>
+  );
 }
-export default Home
+export default Home;
