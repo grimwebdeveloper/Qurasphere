5fc6ceeedcfc7aeb906f73c092cc8e100b7b505e
mdtauhidr507 <mdtauhidr507@gmail.com>
2025-06-26 17:30:01 +0530

    changed and created services cards

--- src/components/global/Banner.jsx
+++ src/components/global/Banner.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 
-const Banner = ({bannerText}) => {
+const Banner = ({ bannerText }) => {
     return (
         <>
-        <div className='w-full bg-gradient-to-r from-blue-500 to-violet-500 text-white '>
-            <div className='max-w-[1280px]  min-h-80 mx-auto  flex justify-center items-center relative overflow-hidden'>
-                <h1 className='text-5xl font-semibold'>{bannerText}</h1>
-                <img src="logo.png" alt="" className='opacity-15 w-104 absolute right-4' />
+            <div className='w-full bg-gradient-to-r from-blue-500 to-violet-500 text-white '>
+                <div className='max-w-[1280px]  min-h-80 mx-auto flex justify-center items-center relative overflow-hidden'>
+                    <h1 className='text-3xl font-semibold md:text-5xl'>{bannerText}</h1>
+                    <img src="logo.png" alt="" className='opacity-15 w-104 absolute right-4' />
+                </div>
             </div>
-        </div>
         </>
     )
 }
